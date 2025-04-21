59fd09cb5928fe40e700298f2bba9e97e667a61c
Queensoft2025 <queensoftsocial@gmail.com>
2025-04-21 23:06:53 +0530

    header image added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,9 @@ const Header = () => {
   return (
     <div className='header'>
         <div className="header-contents">
-          
+          <h2>Welcome to Queensoft</h2>
+          <p>Queensoft in Thoothukudi offers digital marketing, website design, and Google SEO services to help local businesses grow and reach more customers.</p>
+          <button>Explore our sevices</button>
         </div>
     </div>
   )
