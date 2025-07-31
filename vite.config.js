ef6f07fe09308b7d7b216c7bd930f869e884e4df
Queensoft <queensoftsocial@gmail.com>
2025-07-31 22:23:18 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/queensoft_coding/',  // 👈 REQUIRED for GitHub Pages
   plugins: [react()],
 })
